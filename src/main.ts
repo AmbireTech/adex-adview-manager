33cc64c1ea7b6351ce8f2bd53d81f4e24ea03862
Ivo Georgiev <ivo@strem.io>
2019-04-17 18:39:56 +0300

    add BN

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+import { BN } from '../vendor/bn.js';
+
 const MARKET_URL = 'https://market.adex.network';
+
+// opts: target, whitelistedToken, minPerImpression, target, marketURL
+// future: doubleCheck, refreshDebounce, useIdentity, channelWhitelist
+interface AdViewManagerOptions {
+	whitelistedToken: string
+}
 
 export class AdViewManager {
 	private fetch: any;
-	private options: object;
-	// opts: target, whitelistedToken, minPerImpression, target, marketURL
-	// future: doubleCheck, refreshDebounce, useIdentity, channelWhitelist
-	constructor(fetch, opts = {}) {
+	private options: AdViewManagerOptions;
+	constructor(fetch, opts: AdViewManagerOptions) {
 		this.fetch = fetch;
 		this.options = opts;
 	}
@@ -14,8 +20,8 @@ export class AdViewManager {
 		const campaigns = (await this.fetch(`${MARKET_URL}/campaigns`).then(r => r.json()))
 			.filter(x => x.status.name === 'Active' || x.status.name === 'Ready')
 			.filter(x => Array.isArray(x.spec.adUnits) && x.spec.adUnits.length)
-			.filter(x => x.depositAsset === opts.whitelistedToken)
-			//.sort((a, b) => )
+			.filter(x => x.depositAsset === this.options.whitelistedToken)
+			.sort((a, b) => new BN(a.spec.minPerImpression).cmp(new BN(b.spec.minPerImpression)))
 		
 		return "";
 	}
