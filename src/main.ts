3ecba38cdf2484d8adea7f0bec51f0264c919935
Ivo Georgiev <ivo@strem.io>
2019-04-17 17:41:34 +0300

    filter by whitelistedToken

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
 const MARKET_URL = 'https://market.adex.network';
 
-function compareBigNumStrings(a:string, b:string): number {
-	if (a.length > b.length) return 1;
-	if (a.length < b.length) return -1;
-	for (let i=0; i < a.length; i++) {
-		if (parseInt(a[i], 10) > parseInt(b[i], 10)) return 1;
-		if (parseInt(b[i], 10) > parseInt(a[i], 10)) return -1;
-	}
-	return 0;
-}
-
 export class AdViewManager {
 	private fetch: any;
 	private options: object;
-	// opts: target, whitelistedToken, minPerImpression 
+	// opts: target, whitelistedToken, minPerImpression, target, marketURL
+	// future: doubleCheck, refreshDebounce, useIdentity, channelWhitelist
 	constructor(fetch, opts = {}) {
 		this.fetch = fetch;
 		this.options = opts;
@@ -22,7 +13,9 @@ export class AdViewManager {
 	async getAdView(): Promise<string> {
 		const campaigns = (await this.fetch(`${MARKET_URL}/campaigns`).then(r => r.json()))
 			.filter(x => x.status.name === 'Active' || x.status.name === 'Ready')
-			.filter(x => Array.isArray(x.spec.adUnits) && x.spec.adUnits.length);
+			.filter(x => Array.isArray(x.spec.adUnits) && x.spec.adUnits.length)
+			.filter(x => x.depositAsset === opts.whitelistedToken)
+			//.sort((a, b) => )
 		
 		return "";
 	}
