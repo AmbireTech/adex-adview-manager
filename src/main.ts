6661216903fc9ab236468e1564e1cfd35a922113
Ivo Georgiev <ivo@strem.io>
2019-04-17 14:17:57 +0300

    tsconfig and getting from market

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+const MARKET_URL = 'https://market.adex.network';
+
 export class AdViewManager {
 	private options: object;
 	constructor(opts = {}) {
 		this.options = opts;
 	}
 	// @TODO type info for opts
-	getAdView() {
+	async getAdView(): Promise<string> {
+		const campaigns = (await fetch(`${MARKET_URL}/campaigns`).then(r => r.json()))
+			.filter(x => x.status.name === 'Active' || x.status.name === 'Ready')
+			.filter(x => Array.isArray(x.spec.adUnits) && x.spec.adUnits.length);
+
+		return "";
 	}
 }
