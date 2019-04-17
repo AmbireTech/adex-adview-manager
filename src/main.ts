df91620e49532782b6ddf085de24353c6358e79e
Ivo Georgiev <ivo@strem.io>
2019-04-18 00:06:34 +0300

    use STATUS_OK

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { BN } from 'bn.js';
 
 const MARKET_URL = 'https://market.adex.network';
+const STATUS_OK = ['Active', 'Ready'];
 
 interface TargetTag {
 	tag: string,
@@ -40,7 +41,7 @@ export class AdViewManager {
 		// Eligible campaigns
 		const eligible = campaigns
 			.filter(x =>
-				(x.status.name === 'Active' || x.status.name === 'Ready')
+				STATUS_OK.includes(x.status.name)
 				&& Array.isArray(x.spec.adUnits) && x.spec.adUnits.length
 				&& x.depositAsset === this.options.whitelistedToken
 				&& new BN(x.spec.minPerImpression).gte(new BN(this.options.minPerImpression || 0))
