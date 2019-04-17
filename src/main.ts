b43d816d157410eb2c409940d9b48db7b513458d
Ivo Georgiev <ivo@strem.io>
2019-04-17 22:58:09 +0300

    remove BN from vendor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { BN } from './vendor/bn.js';
+import { BN } from 'bn.js';
 
 const MARKET_URL = 'https://market.adex.network';
 
-// opts: target, whitelistedToken, minPerImpression, target, marketURL
+// opts: target, topByPrice, whitelistedToken, minPerImpression, target, marketURL
 // future: doubleCheck, refreshDebounce, useIdentity, channelWhitelist
 interface AdViewManagerOptions {
 	whitelistedToken: string
@@ -16,7 +16,7 @@ export class AdViewManager {
 		this.options = opts;
 	}
 	// @TODO type info for opts
-	async getAdView(): Promise<string> {
+	async getAdView(): Promise<any> {
 		const campaigns = await this.fetch(`${MARKET_URL}/campaigns`).then(r => r.json());
 		const eligible = campaigns
 			.filter(x =>
@@ -24,8 +24,10 @@ export class AdViewManager {
 				&& Array.isArray(x.spec.adUnits) && x.spec.adUnits.length
 				&& x.depositAsset === this.options.whitelistedToken
 			)
-			.sort((a, b) => new BN(a.spec.minPerImpression).cmp(new BN(b.spec.minPerImpression)))
-		
+			.sort((a, b) => new BN(a.spec.minPerImpression).cmp(new BN(b.spec.minPerImpression)));
+		// @TODO get units, sort by minPerImpression
+		// @TODO apply targeting
+		// select the first one
 		return "";
 	}
 }
