5151f8c8fae8a6046c829f952fb0f2a43c316aa6
Ivo Georgiev <ivo@strem.io>
2019-04-17 23:37:54 +0300

    most of the algo done

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ const MARKET_URL = 'https://market.adex.network';
 // opts: target, topByPrice, whitelistedToken, minPerImpression, target, marketURL
 // future: doubleCheck, refreshDebounce, useIdentity, channelWhitelist
 interface AdViewManagerOptions {
-	whitelistedToken: string
+	whitelistedToken: string,
+	topByPrice: number,
 }
 
 export class AdViewManager {
@@ -18,16 +19,29 @@ export class AdViewManager {
 	// @TODO type info for opts
 	async getAdView(): Promise<any> {
 		const campaigns = await this.fetch(`${MARKET_URL}/campaigns`).then(r => r.json());
+
+		// Eligible campaigns
 		const eligible = campaigns
 			.filter(x =>
-				x.status.name === 'Active' || x.status.name === 'Ready'
+				(x.status.name === 'Active' || x.status.name === 'Ready')
 				&& Array.isArray(x.spec.adUnits) && x.spec.adUnits.length
 				&& x.depositAsset === this.options.whitelistedToken
 			)
-			.sort((a, b) => new BN(a.spec.minPerImpression).cmp(new BN(b.spec.minPerImpression)));
+
+		// Map them to units, flatten and sort by price
+		const units = eligible
+			.map(x => x.spec.adUnits.map(unit => ({ unit, channelId: x.id, amount: x.depositAmount })))
+			.reduce((a, b) => a.concat(b), [])
+			.sort((b, a) => new BN(a.amount).cmp(new BN(b.amount)));
+
+		const unitsTop = this.options.topByPrice
+			? units.slice(0, this.options.topByPrice)
+			: units;
+
 		// @TODO get units, sort by minPerImpression
 		// @TODO apply targeting
 		// select the first one
+		console.log(unitsTop)
 		return "";
 	}
 }
