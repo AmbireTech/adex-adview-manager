d318be88080893ff125d56feb5dc48b3efe0dbff
Ivo Georgiev <ivo@strem.io>
2019-04-18 10:36:25 +0300

    tidy up

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,35 @@ function calculateTargetScore(a: Array<TargetTag>, b: Array<TargetTag>): number 
 	}).reduce((a, b) => a + b, 0)
 }
 
+function applyTargeting(campaigns: Array<any>, options: AdViewManagerOptions): Array<any> {
+	// Map them to units, flatten
+	const units = campaigns
+		.map(campaign =>
+			campaign.spec.adUnits.map(unit => ({
+				unit,
+				channelId: campaign.id,
+				minPerImpression: campaign.spec.minPerImpression
+			}))
+		)
+		.reduce((a, b) => a.concat(b), []);
+
+	const unitsByPrice = units
+		.sort((b, a) => new BN(a.minPerImpression).cmp(new BN(b.minPerImpression)));
+
+	const unitsTop = options.topByPrice
+		? unitsByPrice.slice(0, options.topByPrice)
+		: unitsByPrice;
+
+	const unitsByScore = unitsTop
+		.map(x => ({
+			...x,
+			targetingScore: calculateTargetScore(x.unit.targeting, options.targeting || []),
+		}))
+		.sort((a, b) => b - a);
+
+	return unitsByScore;
+}
+
 export class AdViewManager {
 	private fetch: any;
 	private options: AdViewManagerOptions;
@@ -39,40 +68,13 @@ export class AdViewManager {
 		const campaigns = await this.fetch(`${MARKET_URL}/campaigns`).then(r => r.json());
 
 		// Eligible campaigns
-		const eligible = campaigns
-			.filter(x =>
-				STATUS_OK.includes(x.status.name)
-				&& Array.isArray(x.spec.adUnits)
-				&& x.depositAsset === this.options.whitelistedToken
-				&& new BN(x.spec.minPerImpression)
+		const eligible = campaigns.filter(campaign => {
+			return STATUS_OK.includes(campaign.status.name)
+				&& Array.isArray(campaign.spec.adUnits)
+				&& campaign.depositAsset === this.options.whitelistedToken
+				&& new BN(campaign.spec.minPerImpression)
 					.gte(new BN(this.options.minPerImpression || 0))
-			)
-
-		// Map them to units, flatten
-		const units = eligible
-			.map(campaign =>
-				campaign.spec.adUnits.map(unit => ({
-					unit,
-					channelId: campaign.id,
-					minPerImpression: campaign.spec.minPerImpression
-				}))
-			)
-			.reduce((a, b) => a.concat(b), []);
-
-		const unitsByPrice = units
-			.sort((b, a) => new BN(a.minPerImpression).cmp(new BN(b.minPerImpression)));
-
-		const unitsTop = this.options.topByPrice
-			? unitsByPrice.slice(0, this.options.topByPrice)
-			: unitsByPrice;
-
-		const unitsByScore = unitsTop
-			.map(x => ({
-				...x,
-				targetingScore: calculateTargetScore(x.unit.targeting, this.options.targeting || []),
-			}))
-			.sort((a, b) => b - a);
-
-		return unitsByScore;
+		});
+		return applyTargeting(eligible, this.options);
 	}
 }
