11840c5c4933d33140c27a0c258375008cff29ba
Ivo paunov <ivo.paunov@gmail.com>
2019-08-01 13:46:26 +0300

    add adex icon

--- src/main.ts
+++ src/main.ts
@@ -114,15 +114,39 @@ function videoHtml({ evBody, onLoadCode, size, imgUrl, mediaMime }): string {
 		`</video>`
 }
 
+function adexIcon(): string {
+	return  `<a href="https://adex.network" target="_blank" rel="noopener noreferrer"
+			style="position: absolute; top: 0; right: 0;"
+		>`
+		+ `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="10px"
+				height="16.2px" viewBox="0 0 10 16.2" style="enable-background:new 0 0 10 16.2;" xml:space="preserve">
+				<style type="text/css">
+					.st0{fill:#1B75BC;}
+				</style>
+				<defs>
+				</defs>
+				<title>ADX-icon</title>
+				<path class="st0" d="M10,11.3L6.9,8.1L10,5L8.1,3.1L5,6.3L1.9,3.1L0,5l3.1,3.1L0,11.3l1.9,1.9L5,10l3.1,3.1L10,11.3z M3.9,1.1
+					L2.4,2.6l1.5,1.5L5,3.1l1.1,1.1l1.5-1.5L6.1,1.1L5,0L3.9,1.1 M3.9,15.2l-1.6-1.6l1.5-1.5L5,13.2l1.1-1.1l1.5,1.5l-1.6,1.6L5,16.2
+					L3.9,15.2"/>
+			</svg>`
+		+ `</a>`
+}
+
 function getUnitHTML({ width, height }: AdViewManagerOptions, { unit, evBody = '', onLoadCode = '' }): string {
 	const imgUrl = normalizeUrl(unit.mediaUrl)
 	const isVideo = (unit.mediaMime || '').split('/')[0] === 'video'
 	const size = width && height ? `width="${width}" height="${height}" ` : ''
-	return `<a href="${unit.targetUrl}" target="_blank" rel="noopener noreferrer">`
+	return `<div
+			style="position: relative; overflow: hidden;"
+		>` 	
+		+ `<a href="${unit.targetUrl}" target="_blank" rel="noopener noreferrer">`
 		+ (isVideo
 			? videoHtml({ evBody, onLoadCode, size, imgUrl, mediaMime: unit.mediaMime })
 			: imageHtml({ evBody, onLoadCode, size, imgUrl }))
 		+ `</a>`
+		+ adexIcon()
+		+ `</div>`
 }
 
 function getHTML(options: AdViewManagerOptions, { unit, channelId, validators }): string {
