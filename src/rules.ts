5e206da9616bd9092294caef4f06f7d9a1d84326
Ivo Georgiev <ivo@strem.io>
2020-05-25 19:27:36 +0300

    rules: implement eq, some idea of how to implement bignumbers

--- src/rules.ts
+++ src/rules.ts
@@ -1,3 +1,4 @@
+import { BN } from 'bn.js'
 
 // has 3 outcomes: does nothing, mutates output, throws error
 // eval errors: TypeError, UndefinedVars
@@ -30,10 +31,15 @@ export function evaluate(input: any, output: any, rule: any) {
 	} else if (rule.do) {
 		return rule.do.map(evalRule)
 	// lists
+	// @TODO: document that the lists functions do not support BigNumbers within them
 	} else if (rule.in) {
 		const a = evalRule(rule.in[0])
 		const b = evalRule(rule.in[1])
 		return a.includes(b)
+	} else if (rule.nin) {
+		const a = evalRule(rule.nin[0])
+		const b = evalRule(rule.nin[1])
+		return !a.includes(b)
 	} else if (rule.intersects) {
 		const a = evalRule(rule.intersects[0])
 		const b = evalRule(rule.intersects[1])
@@ -48,11 +54,21 @@ export function evaluate(input: any, output: any, rule: any) {
 		// @TODO: separate error for trying to set undefined
 		const prevType = typeof output[key]
 		const value = evalRule(rule.set[1])
-		// @TODO type check
 		output[key] = assertType(value, prevType)
 	// utilities
 	} else if (rule.onlyShowIf) {
 		if (!evalRule(rule.onlyShowIf)) output.show = false
+	// comparison
+	} else if (rule.eq) {
+		const a = evalRule(rule.eq[0])
+		const b = evalRule(rule.eq[1])
+		if (a instanceof BN) return a.eq(new BN(b))
+		if (b instanceof BN) return b.eq(new BN(a))
+		return a === b
+	} else if (rule.lt) {
+	} else if (rule.gt) {
+	} else if (rule.gte) {
+	
 	// logic
 	// @TODO: typechecking and type errors
 	} else if (rule.not) {
@@ -61,6 +77,14 @@ export function evaluate(input: any, output: any, rule: any) {
 		return evalRule(rule.or[0]) || evalRule(rule.or[1])
 	} else if (rule.and) {
 		return evalRule(rule.and[0]) && evalRule(rule.and[1])
+	// math
+	} else if (rule.div) {
+	} else if (rule.mul) {
+	} else if (rule.mod) {
+	} else if (rule.add) {
+	} else if (rule.sub) {
+	} else if (rule.max) {
+	} else if (rule.min) {
 	}
 }
 
@@ -85,4 +109,12 @@ function assertType(value: any, typeName: string): any {
 		}
 	}
 	return value
+}
+
+// @TODO types
+function withNumbers(numbers: any, onNumbers: any, onBNs: any): any {
+	// min 2 args
+	// case when there are two args
+	// case when there are multiple
+	// case when numbers, case when BN
 }
