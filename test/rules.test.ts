0bd81a8cdae739dc575cf5a47ff3dfe290c31e49
Ivo Georgiev <ivo@strem.io>
2020-05-25 23:38:39 +0300

    rules: lists tests

--- test/rules.test.ts
+++ test/rules.test.ts
@@ -3,6 +3,11 @@ import * as test from 'tape'
 import { BN } from 'bn.js'
 
 const evalPure = x => evaluate({}, {}, x)
+const evalToOutput = x => {
+	let output = { foo: 1, bar: 2 }
+	evaluate({}, output, x)
+	return output
+}
 
 console.log(evaluate({}, {}, { bn: '100000' }))
 console.log(evaluate({}, {}, { ifElse: [{endsWith: ['foo', 'oo']}, 'cool and good', 'not'] }))
@@ -12,6 +17,35 @@ console.log(evaluate({}, {}, { ifElse: [{endsWith: ['foo', 'oo']}, 'cool and goo
    { eq: [{ get: 'publisherId' }, '0xd5860D6196A4900bf46617cEf088ee6E6b61C9d6'] },
    { set: ['price.IMPRESSION', { mul: [1.5, { get: 'price.IMPRESSION' }] }] }
 ] }))*/
+
+// flow control
+test('flow control', t => {
+	t.deepEqual(evalToOutput({ if: [true, { set: ['bar', 3] }] }), { foo: 1, bar: 3 })
+	t.deepEqual(evalToOutput({ ifNot: [false, { set: ['bar', 3] }] }), { foo: 1, bar: 3 })
+	t.deepEqual(evalToOutput({ ifElse: [false, { set: ['bar', 3] }, { set: ['bar', 4] }] }), { foo: 1, bar: 4 })
+	t.deepEqual(evalToOutput({ do: [{ set: ['foo', 3] }, { set: ['bar', 4] }] }), { foo: 3, bar: 4 })
+	t.end()
+})
+
+test('flow control: errors', t => {
+	t.throws(() => evalPure({ ifElse: [false, { set: ['bar', 3] }] }), RuleEvalError)
+	t.end()
+})
+
+// lists (arrays)
+test('lists', t => {
+	t.equal(evalPure({ in: [[3, 4, 2], 2] }), true)
+	t.equal(evalPure({ in: [[3, 4, 2], 15] }), false)
+	t.equal(evalPure({ nin: [[3, 4, 2], 2] }), false)
+	t.equal(evalPure({ nin: [[3, 4, 2], 15] }), true)
+
+	t.equal(evalPure({ intersects: [[2, 15], [3, 4, 2]] }), true)
+	t.equal(evalPure({ intersects: [[15, 18], [3, 4, 2]] }), false)
+
+	t.equal(evalPure({ at: [[15, 17, 5, 1], 2] }), 5)
+
+	t.end()
+})
 
 
 // logic
@@ -33,3 +67,11 @@ test('logic functions: errors', t => {
 	t.throws(() => evalPure({ or: true }), RuleEvalError)
 	t.end()
 })
+
+// set/get
+test('set/get: errors', t => {
+	t.throws(() => evalToOutput({ set: ['foo', 'string'] }), RuleEvalError, 'cannot change type of an output variable')
+	t.end()
+})
+
+
