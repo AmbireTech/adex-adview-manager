850d394df4d25956da31d46624dc073efecca16e
Ivo Georgiev <ivo@strem.io>
2020-05-29 22:32:40 +0300

    WIP: total refactor/rewrite for AIP31

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { BN } from 'bn.js'
 
 export const IPFS_GATEWAY = 'https://ipfs.moonicorn.network/ipfs/'
+// @TODO unused
 export const GLOBAL_MIN_PER_IMPRESSION = new BN('20000000000000')
 
 // How much time to wait before sending out an impression event
@@ -9,114 +10,29 @@ const WAIT_FOR_IMPRESSION = 8000
 
 const defaultOpts = {
 	marketURL: 'https://market.moonicorn.network',
-	acceptedStates: ['Active', 'Ready'],
-	minPerImpression: '1',
-	minTargetingScore: 0,
-	// SAI and DAI
 	whitelistedTokens: ['0x6B175474E89094C44Da98b954EedeAC495271d0F'],
-	topByScore: 12,
-	topByPrice: 6,
-	randomize: true,
 	disableVideo: false,
-}
-
-interface TargetTag {
-	tag: string,
-	score: number
 }
 
 type BigNumStr = string
 interface AdViewManagerOptions {
 	// Defaulted via defaultOpts
 	marketURL: string,
-	acceptedStates: Array<string>,
-	minPerImpression: BigNumStr,
-	minTargetingScore: number,
-	randomize: boolean,
+	marketSlot: string,
 	// Must be passed (except the ones with ?)
 	publisherAddr: string,
 	// All passed tokens must be of the same price and decimals, so that the amounts can be accurately compared
-	// e.g. SAI and DAI
 	whitelistedTokens?: Array<string>,
-	whitelistedType?: string,
-	topByPrice?: number,
-	topByScore?: number,
-	targeting?: Array<TargetTag>,
 	width?: number,
 	height?: number,
-	fallbackUnit?: string,
 	disableVideo?: boolean,
-	marketSlot?: string,
-	// Which referrer prefixes are ok
-	acceptedReferrers?: Array<string>,
 }
 
-export function calculateTargetScore(a: Array<TargetTag>, b: Array<TargetTag>): number {
-	return a.map(x => {
-		const match = b.find(y => y.tag === x.tag && y.score)
-		if (match) {
-			return x.score * match.score
-		}
-		return 0
-	}).reduce((a, b) => a + b, 0)
-}
-
-export function applySelection(campaigns: Array<any>, options: AdViewManagerOptions): Array<any> {
-	const eligible = campaigns.filter(campaign => {
-		const minPerImpression = new BN(campaign.spec.minPerImpression)
-		return options.acceptedStates.includes(campaign.status.name)
-			&& (campaign.spec.activeFrom || 0) < Date.now()
-			&& Array.isArray(campaign.spec.adUnits)
-			&& options.whitelistedTokens.includes(campaign.depositAsset)
-			&& minPerImpression.gte(new BN(options.minPerImpression))
-			&& minPerImpression.gte(GLOBAL_MIN_PER_IMPRESSION)
-			&& campaign.creator !== options.publisherAddr
-	})
-
-	// Map them to units, flatten
-	const units = eligible
-		.map(campaign =>
-			campaign.spec.adUnits.map(unit => ({
-				unit,
-				channelId: campaign.id,
-				validators: campaign.spec.validators,
-				minTargetingScore: unit.minTargetingScore || campaign.spec.minTargetingScore || 0,
-				minPerImpression: campaign.spec.minPerImpression,
-			}))
-		)
-		.reduce((a, b) => a.concat(b), [])
-
-	const unitsFiltered = options.whitelistedType
-		? units.filter(x =>
-			x.unit.type === options.whitelistedType
-			&& !(options.disableVideo && isVideo(x.unit))
-		)
-		: units
-
-	const unitsByScore = unitsFiltered
-		.map(x => ({
-			...x,
-			targetingScore: calculateTargetScore(x.unit.targeting, options.targeting || []),
-		}))
-		.filter(x =>
-			x.targetingScore >= options.minTargetingScore
-			&& x.targetingScore >= x.minTargetingScore
-		)
-		.sort((a, b) => b.targetingScore - a.targetingScore)
-
-	const unitsTop = options.topByScore
-		? unitsByScore.slice(0, options.topByScore)
-		: unitsByScore
-
-	const unitsByPrice = unitsTop
-		.sort((b, a) => new BN(a.minPerImpression).cmp(new BN(b.minPerImpression)))
-
-	const unitsTopByPrice = options.topByPrice
-		? unitsByPrice.slice(0, options.topByPrice)
-		: unitsByPrice
-
-	return unitsTopByPrice
-		.sort((a, b) => b.targetingScore - a.targetingScore)
+interface Unit {
+	id: string,
+	mediaUrl: string,
+	mediaMime: string,
+	targetUrl: string
 }
 
 export function normalizeUrl(url: string): string {
@@ -153,7 +69,7 @@ function adexIcon(): string {
 		+ `</a>`
 }
 
-function isVideo(unit: any): boolean {
+function isVideo(unit: Unit): boolean {
 	return (unit.mediaMime || '').split('/')[0] === 'video'
 }
 
@@ -174,7 +90,7 @@ function getUnitHTML({ width, height }: AdViewManagerOptions, { unit, onLoadCode
 
 export function getHTML(options: AdViewManagerOptions, { unit, channelId, validators }): string {
 	const adSlotCode = options.marketSlot ? `, adSlot: '${options.marketSlot}'` : ''
-	const getBody = (evType) => `JSON.stringify({ events: [{ type: '${evType}', publisher: '${options.publisherAddr}', adUnit: '${unit.ipfs}', ref: document.referrer${adSlotCode} }] })`
+	const getBody = (evType) => `JSON.stringify({ events: [{ type: '${evType}', publisher: '${options.publisherAddr}', adUnit: '${unit.id}', ref: document.referrer${adSlotCode} }] })`
 	const getFetchCode = (evType) => `var fetchOpts = { method: 'POST', headers: { 'content-type': 'application/json' }, body: ${getBody(evType)} };` + validators
 		.map(({ url }) => {
 			const fetchUrl = `${url}/channel/${channelId}/events?pubAddr=${options.publisherAddr}`
@@ -190,83 +106,36 @@ export class AdViewManager {
 	private fetch: any
 	private options: AdViewManagerOptions
 	private timesShown: { [key: string]: number }
-	private optsLoaded: boolean
 	private getTimesShown(channelId: string): number {
 		return this.timesShown[channelId] || 0
 	}
 	constructor(fetch, opts: AdViewManagerOptions) {
 		this.fetch = fetch
 		this.options = { ...defaultOpts, ...opts }
+		// @TODO unused
 		this.timesShown = {}
-		this.optsLoaded = false
 	}
-	async loadOptionsFromMarket() {
-		const opts = this.options
-
-		if (!this.optsLoaded && opts.marketSlot) {
-				const url = `${opts.marketURL}/slots/${opts.marketSlot}`
-				const { slot, acceptedReferrers } = await this.fetch(url)
-					.then(r => {
-						if(r.status >= 200 && r.status < 400){
-							return r.json()
-						} else {
-							return {}
-						}
-					})
-				// If this is an object, it is a mapping of tokenAddr->value, but since a slot always uses tokens
-				// of the same price/decimals, all values should be the same
-				const resMinPerImpression: any = typeof slot.minPerImpression === 'string'
-					? slot.minPerImpression
-					: Object.values(slot.minPerImpression || {})[0]
-				const optsOverride = {
-					fallbackUnit: slot.fallbackUnit || opts.fallbackUnit,
-					minPerImpression: resMinPerImpression || opts.minPerImpression,
-					minTargetingScore: slot.minTargetingScore || opts.minTargetingScore,
-					targeting: slot.tags || opts.targeting,
-					acceptedReferrers: acceptedReferrers || opts.acceptedReferrers
-				}
-
-				this.options = { ...opts, ...optsOverride }
-				this.optsLoaded = true
-		}
-	}
-
-	async getAdUnits(): Promise<any> {
-		const states = `status=${this.options.acceptedStates.join(',')}`
-		const publisherLimit = `limitForPublisher=${this.options.publisherAddr}`
-		const url = `${this.options.marketURL}/campaigns/with-targeting?${states}&${publisherLimit}`
-		const { campaigns, targeting } = await this.fetch(url).then(r => r.json())
-		this.options.targeting = this.options.targeting.concat(targeting)
-		return applySelection(campaigns, this.options)
-	}
-	async getFallbackUnit(): Promise<any> {
-		const { fallbackUnit } = this.options
-		if (!fallbackUnit) return null
-		const url = `${this.options.marketURL}/units/${this.options.fallbackUnit}`
-		const result = await this.fetch(url).then(r => r.json())
-		return result.unit
+	async getMarketDemandResp(): Promise<any> {
+		// @TODO remove dev mode (localhost)
+		const marketURL = 'http://localhost:3012' || this.options.marketURL
+		const depositAsset = this.options.whitelistedTokens.length === 1 ? `&depositAsset=${this.options.whitelistedTokens[0]}` : ``
+		const pubPrefix = this.options.publisherAddr.slice(2, 10)
+		const url = `${marketURL}/units-for-slot/${this.options.marketSlot}?pubPrefix=${pubPrefix}${depositAsset}`
+		return this.fetch(url).then(r => r.json())
 	}
 	async getNextAdUnit(): Promise<any> {
-		await this.loadOptionsFromMarket()
-		const units = await this.getAdUnits()
-		if (units.length === 0) {
-			const fallbackUnit = await this.getFallbackUnit()
-			if (fallbackUnit) {
-				return { ...fallbackUnit, html: getUnitHTML(this.options, { unit: fallbackUnit }) }
-			} else {
-				return null
-			}
+		const { campaigns, targetingInputBase, acceptedReferrers, fallbackUnit } = await this.getMarketDemandResp()
+		const unitsWithPrice = campaigns
+			.map(x => x.unitsWithPrice)
+			.reduce((a, b) => a.concat(b), [])
+			.filter(x => !(this.options.disableVideo && isVideo(x.unit)))
+			.sort((b, a) => new BN(a.price).cmp(new BN(b.price)))
+		const unit = unitsWithPrice[0] ? unitsWithPrice[0].unit : fallbackUnit
+		if (!unit) return null
+		return {
+			unit,
+			acceptedReferrers,
+			html: getUnitHTML(this.options, { unit }),
 		}
-
-		const min = units
-			.map(({ channelId }) => this.getTimesShown(channelId))
-			.reduce((a, b) => Math.min(a, b))
-		const leastShownUnits = units
-			.filter(({ channelId }) => this.getTimesShown(channelId) === min)
-		const next = this.options.randomize ?
-			leastShownUnits[Math.floor(Math.random() * leastShownUnits.length)]
-			: leastShownUnits[0]
-		this.timesShown[next.channelId] = this.getTimesShown(next.channelId) + 1
-		return { ...next, html: getHTML(this.options, next) }
 	}
 }
