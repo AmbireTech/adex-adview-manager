4011bec69d573bdd65f368a3fe100ddf9ea8b587
Ivo Georgiev <ivo@strem.io>
2020-05-31 23:10:11 +0300

    main-browser: safeJSONParse to ensure localStorage corruptions are not a problem

--- main-browser.js
+++ main-browser.js
@@ -3,12 +3,22 @@ const { AdViewManager } = require('./lib/main')
 // limiting to 2 views per last 10 seconds
 const MAX_VIEWS_CAP = { timeframe: 10000, limit: 2 }
 
-function initWithOptions(options) {
-	function collapse() {
-		// Collapse the space
-		window.parent.postMessage({ adexHeight: 0 }, "*")
+function safeJSONParse(json, defaultVal) {
+	if (!json) return defaultVal
+	try {
+		return JSON.parse(json)
+	} catch(e) {
+		console.error(e)
+		return defaultVal
 	}
+}
 
+function collapse() {
+	// Collapse the space
+	window.parent.postMessage({ adexHeight: 0 }, "*")
+}
+
+function initWithOptions(options) {
 	// basic headless detection
 	if (navigator.webdriver || !(Array.isArray(navigator.languages) && navigator.languages.length)) {
 		collapse()
@@ -16,7 +26,7 @@ function initWithOptions(options) {
 	}
 
 	const now = Date.now()
-	let views = JSON.parse(localStorage.views || '[]')
+	let views = safeJSONParse(localStorage.views, [])
 	views = views.filter(x => now-x < MAX_VIEWS_CAP.timeframe)
 	if (views.length >= MAX_VIEWS_CAP.limit) {
 		console.log('AdEx: ads per page limit exceeded')
@@ -38,7 +48,7 @@ function initWithOptions(options) {
 
 	// construct the AdView manager with existing history, select the next ad unit, display it
 	const historyKey = `history_${options.publisherAddr}`
-	const history = JSON.parse(localStorage[historyKey] || '[]')
+	const history = safeJSONParse(localStorage[historyKey], [])
 	const mgr = new AdViewManager((url, o) => fetch(url, o), options, history)
 	mgr.getNextAdUnit().then(u => {
 		if (Array.isArray(u.acceptedReferrers)
