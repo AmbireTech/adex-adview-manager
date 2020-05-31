eeeee67ec72b20fffa8d61ef0741d0c3c8bf5805
Ivo Georgiev <ivo@strem.io>
2020-05-31 12:38:01 +0300

    dev banners and acceptedReferrers logic fixed

--- main-browser.js
+++ main-browser.js
@@ -44,16 +44,15 @@ function initWithOptions(options) {
 	}
 	const mgr = new AdViewManager((url, o) => fetch(url, o), options)
 	mgr.getNextAdUnit().then(u => {
-		if (Array.isArray(mgr.options.acceptedReferrers)
+		if (Array.isArray(u.acceptedReferrers)
 			&& document.referrer
 			&& !document.referrer.startsWith('https://localhost:8080')
-			&& !mgr.options.acceptedReferrers.some(ref => document.referrer.startsWith(ref))
+			&& !u.acceptedReferrers.some(ref => document.referrer.startsWith(ref))
 		) {
 			// @TODO: more correct detection
-			if (document.referrer.includes('/localhost') && options.whitelistedType.startsWith('legacy_')) {
-				const size = options.whitelistedType.split('_')[1]
-				const sizeSplit = size.split('x')
-				document.body.innerHTML = `<img src="/dev-banners/${size}.jpg" alt="AdEx development banner" width="${sizeSplit[0]}" height="${sizeSplit[1]}">`
+			if (document.referrer.includes('/localhost')) {
+				const size = `${mgr.options.width}x${mgr.options.height}`
+				document.body.innerHTML = `<img src="/dev-banners/${size}.jpg" alt="AdEx development banner" width="${mgr.options.width}" height="${mgr.options.height}">`
 			} else {
 				console.log(`AdEx: ad slot installed on wrong website (referrer)`)
 				collapse()
