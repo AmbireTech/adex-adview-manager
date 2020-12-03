fb7e2ff1df12d86a3c2ff6f7243e2294ec0215b6
Rangel Stoilov <rangelstoilov@gmail.com>
2020-12-03 17:56:45 +0200

    change webpack config for multiple entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
-const path = require('path');
-const SriPlugin = require('webpack-subresource-integrity');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const SriPlugin = require('webpack-subresource-integrity')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './main-browser.js',
-  output: {
-    filename: `main.js?t=${Date.now()}`,
-    path: path.resolve(__dirname, 'dist.browser'),
-    crossOriginLoading: 'anonymous',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateParameters: {
-        title: 'AdEx Adview',
-      },
-      template: 'index.ejs',
-    }),
-    new SriPlugin({
-      hashFuncNames: ['sha256', 'sha384'],
-      enabled: true,
-    }),
-  ],
-};
+	entry: { main: './main-browser.js', moon: './moon.js' },
+	output: {
+		filename: `[name].js?t=${Date.now()}`,
+		path: path.resolve(__dirname, 'dist.browser'),
+		crossOriginLoading: 'anonymous'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			templateParameters: {
+				title: 'AdEx Adview'
+			},
+			chunks: ['main'],
+			template: 'index.ejs',
+			filename: 'index.html'
+		}),
+		new HtmlWebpackPlugin({
+			templateParameters: {
+				title: 'Example JS import & div-s'
+			},
+			chunks: ['moon'],
+			template: 'js-example.ejs',
+			filename: 'js-example.html'
+		}),
+		new SriPlugin({
+			hashFuncNames: ['sha256', 'sha384'],
+			enabled: true
+		})
+	]
+}
